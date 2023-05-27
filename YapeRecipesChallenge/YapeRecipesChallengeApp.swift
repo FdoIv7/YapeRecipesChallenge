41bed7d10df960216ce689b3ac653bbc91f9b3a0
Fernando Perez <fdoiv7@gmail.com>
2023-05-27 14:40:36 -0600

    Initial Commit

---
+++ YapeRecipesChallenge/YapeRecipesChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YapeRecipesChallengeApp.swift
+//  YapeRecipesChallenge
+//
+//  Created by Fernando Ives on 27/05/23.
+//
+
+import SwiftUI
+
+@main
+struct YapeRecipesChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
